ffcf222e91f063395e6315e69aa8ebb020c0631e
Whythree <whythree1.dollars@gmail.com>
2025-03-12 21:29:26 +0100

    Refactoring count words to stats file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from stats import count_words
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_content = f.read()
         return file_content
 
-def main():
-    print(get_book_text("./books/frankenstein.txt"))
 
+
+def main():
+    print(f"{count_words(get_book_text("./books/frankenstein.txt"))} words found in the document.")
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(text):
+    array_of_words = text.split()
+    return len(array_of_words)
